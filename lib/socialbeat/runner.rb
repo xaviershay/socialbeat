21dd85d3f8a2e5432f3b99741a1c0e5ac3aa9352
Xavier Shay <xavier@rhnh.net>
2008-10-09 08:06:07 -0700

    Fix the physics time step for deterministicity, use a real value for the draw time step

--- lib/socialbeat/runner.rb
+++ lib/socialbeat/runner.rb
@@ -1,9 +1,13 @@
 module SocialBeat
   class Runner
+    PHYSICS_STEP = 1.0 / 100 # 100 FPS
+
     def initialize
       @canvas = Canvas::OpenGl.new
       @canvas.on_idle    = method(:update)
       @canvas.on_display = method(:draw)
+
+      @physics_accum = 0.0
     end
     
     def run(artist_file)
@@ -19,17 +23,26 @@ module SocialBeat
 
     def update
       now = Time.now
-      u = now - (@last_time || now) 
-      @last_time = now
+      u = now - (@last_physics_time || now) 
+      @last_physics_time = now
       
       @artist_loader.update(u)
 
-      @artist_loader.current_instance.update(@canvas, @env, 0.01)
+      @physics_accum += u
+      # This loop forces a fixed physics update step, giving us deterministic behaviour 
+      while @physics_accum >= PHYSICS_STEP 
+        @artist_loader.current_instance.update(@canvas, @env, PHYSICS_STEP)
+        @physics_accum -= PHYSICS_STEP
+      end
       @canvas.refresh
     end
 
     def draw
-      @artist_loader.current_instance.draw(@canvas, @env, 0.01)
+      now = Time.now
+      u = now - (@last_draw_time || now) 
+      @last_draw_time = now
+
+      @artist_loader.current_instance.draw(@canvas, @env, u)
     end
   end
 end
