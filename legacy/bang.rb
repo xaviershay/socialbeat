993fc829e5163483af01d6be2ccf961e94b94b95
Xavier Shay <xavier@rhnh.net>
2008-10-10 22:26:52 -0700

    Remove legacy/, there's nothing worth keeping in here anymore

--- legacy/bang.rb
+++
@@ -1,4 +0,0 @@
-# Bang that drum!
-(10*rand).to_i.times do
-  `curl -s localhost:2000/bangs`
-end
--- legacy/tick.rb
+++
@@ -1,5 +0,0 @@
-# Counts a very unsteady 120bpm
-while(true) do
-  `curl -s localhost:2000/beats`
-  sleep 0.5
-end
