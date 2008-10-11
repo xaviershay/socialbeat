558b2e47d620b821e1e22d7f07e294bea5f9e531
Xavier Shay <xavier@rhnh.net>
2008-10-10 22:29:12 -0700

    Fix default implementation in Artists

--- lib/socialbeat/artist.rb
+++ lib/socialbeat/artist.rb
@@ -1,6 +1,9 @@
 module SocialBeat
   class Artist
-    def update(canvas, env, u)
+    def setup_environment(env)
+    end
+
+    def update(events, canvas, env, u)
     end
 
     def draw(canvas, env, u)
